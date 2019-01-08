14ed032a19653a4f7c8eabb1f259769a99cd9562
Jakub Wiśniewski <jakwis93@gmail.com>
2019-01-08 23:09:08 +0100

    js file added

---
+++ main.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal(".anim");
+ScrollReavel({ reset: true });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
