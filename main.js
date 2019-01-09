393c2a8219f2bd92b95a62df7b747f54c1c818f1
Jakub Wiśniewski <jakwis93@gmail.com>
2019-01-09 18:47:07 +0100

    Full commit

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-ScrollReveal().reveal(".anim");
-ScrollReavel({ reset: true });
+$(document).ready(function () {
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+  ScrollReveal().reveal(".anim");
+  ScrollReavel({ reset: true });
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  };
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  };
+});
